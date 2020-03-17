99f6d8108d2af58c14a37258bfe15e88fc7dba12
Tibor Billes <tbilles@gmx.com>
2020-03-17 17:31:26 +0100

    begint tbilles mods

--- modinfo.lua
+++ modinfo.lua
@@ -2,10 +2,11 @@ return {
   name = 'Balanced Annihilation',
   description = 'Balanced Annihilation',
   shortname = 'BA',
-  version = '$VERSION',
+  version = 'V10.24-tbilles~1',
   mutator = 'Official',
   game = 'Balanced Annihilation',
   shortGame = 'BA',
   modtype = 1,
   url = "https://www.balancedannihilation.com",
 }
+
