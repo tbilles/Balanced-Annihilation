3da3cf2dd231f882d4540fe775445724e5d44e11
Tibor Billes <tbilles@gmx.com>
2020-06-07 21:08:12 +0200

    V10.24-tbilles~5

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name = 'Balanced Annihilation',
   description = 'Balanced Annihilation',
   shortname = 'BA',
-  version = 'V10.24-tbilles~4',
+  version = 'V10.24-tbilles~5',
   mutator = 'Official',
   game = 'Balanced Annihilation',
   shortGame = 'BA',
